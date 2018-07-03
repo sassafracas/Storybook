e31872ee54658fe4780840ae46fd5962aee6c41e
github username <torpid@gmail.com>
2018-07-03 16:36:36 -0400

    added semantic & styled navbar

--- stories-frontend/src/NavBar.js
+++ stories-frontend/src/NavBar.js
@@ -1,52 +1,55 @@
-import React, { Fragment } from "react";
-import { NavLink, Switch } from 'react-router-dom';
+import React, { Fragment, Component } from "react";
+import { NavLink, Link, Switch } from 'react-router-dom';
 import Adapter from "./Adapter"
+import { Menu, Segment } from 'semantic-ui-react';
 
-const NavBar = (props) => {
-  console.log(props)
+export default class NavBar extends Component {
+
+  state = {
+    activeItem: "My Stories"
+  }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+render(){
+
+  const { activeItem } = this.state
+
   return (
-    <header>
+        <Fragment>
          { Adapter.isLoggedIn() ?
-           <Fragment>
-             <NavLink
-               activeStyle={{
-                 fontWeight: 'bold',
-                 color: 'red'
-               }}
-               exact to="/my-stories">My Stories</NavLink>
-             <NavLink
-               activeStyle={{
-                 fontWeight: 'bold',
-                 color: 'red'
-               }}
-               exact to="/explore">Explore</NavLink>
-             <NavLink activeStyle={{
-                 fontWeight: 'bold',
-                 color: 'red'
-               }}exact to="/upload">Upload</NavLink>
-             <button onClick={() => {
-                 Adapter.logout();
-                 props.routeInfo.history.push("/login");
-               }}>Logout</button>
-           </Fragment>
-           :
-             <Fragment>
-               <NavLink
-                 activeStyle={{
-                   fontWeight: 'bold',
-                   color: 'red'
-                 }}
-                 exact to="/register">Registration</NavLink>
-               <NavLink
-                 activeStyle={{
-                   fontWeight: 'bold',
-                   color: 'red'
-                 }}
-                 exact to="/login">Login</NavLink>
-             </Fragment>
-         }
-     </header>
-   )
-}
+      <Menu pointing secondary>
+           <Menu.Menu>
+             <Menu.Item name="My Stories" active={activeItem === 'My Stories'} onClick={this.handleItemClick}>
+               <Link exact to="/my-stories">My Stories</Link>
+             </Menu.Item>
+             <Menu.Item name="Explore" active={activeItem === 'Explore'} onClick={this.handleItemClick}>
+               <Link exact to="/explore">Explore</Link>
+             </Menu.Item>
+             <Menu.Item name="Upload" active={activeItem === 'Upload'} onClick={this.handleItemClick}>
+               <Link exact to="/upload">Upload</Link>
+             </Menu.Item>
+             </Menu.Menu>
+             <Menu.Menu position="right">
+               <Menu.Item name="Logout" active={activeItem === 'Logout'} onClick={() => {
+                   this.handleItemClick;
+                   Adapter.logout();
+                   this.props.routeInfo.history.push("/login");
+                 }}>
+               </Menu.Item>
+             </Menu.Menu>
+      </Menu>
 
-export default NavBar;
+           :
+             <Menu>
+               <Menu.Item name="Register" active={activeItem === 'Register'} onClick={this.handleItemClick}>
+                 <Link exact to="/register">Registration</Link>
+               </Menu.Item>
+               <Menu.Item name="Login" active={activeItem === 'Login'} onClick={this.handleItemClick}>
+                 <Link exact to="/login">Login</Link>
+               </Menu.Item>
+             </Menu>
+         }
+</Fragment>
+   )
+ }
+}
