cf5b43735a52e231bf44f84a6f6d873e60855c53
github username <torpid@gmail.com>
2018-07-03 16:36:36 -0400

    trying to get infinite scrolling to work

--- stories-frontend/src/MyStories.js
+++ stories-frontend/src/MyStories.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import Adapter from "./Adapter"
-import PhotoStory from "./PhotoStory"
+import Adapter from "./Adapter";
+import PhotoStory from "./PhotoStory";
 
 //When I click on a story title, redirects to my-stories/${story.id}
 //Renders a PhotoDetails component with photostory & photos
 class MyStories extends Component {
 
   state = {
-    photostories: [],
+    photostories: []
   }
 
   componentDidMount(){
--- stories-frontend/src/PhotoStory.js
+++ stories-frontend/src/PhotoStory.js
@@ -2,27 +2,47 @@ import React, { Component } from "react";
 import Adapter from "./Adapter";
 import Photo from "./Photo";
 import { Link } from 'react-router-dom';
-import { Segment } from 'semantic-ui-react';
+import { Segment, Visibility, Loader } from 'semantic-ui-react';
 
 class PhotoStory extends Component {
+
+  state = {
+    show: false
+  }
 
   deletePhotostory = () => {
     Adapter.deleteOnePhotostory(this.props.photostory.id).then(r => console.log(r))
   }
 
+  showPhotostory = () => {
+    this.setState ({
+      show: true
+    }, () => console.log("visible"))
+  }
+
   render() {
    console.log("photostory props ", this.props)
-    return (
-      <Segment vertical>
-        <Link to={{
-            pathname: `/my-stories/${this.props.photostory.id}`,
-            state: {...this.props}
-          }}><h3>{this.props.photostory.title}</h3></Link>
-          <button onClick={this.deletePhotostory}>Delete Story</button>
-          <Photo photos={this.props.photostory.photos}/>
-      </Segment>
-    )
-  }
+
+
+        if (!this.state.show) {
+          return (<Visibility onTopVisible={this.showPhotostory}>
+                    <Loader active inline="centered" />
+                  </Visibility>)
+          }
+          return (<Segment vertical>
+            <Link to={{
+                pathname: `/my-stories/${this.props.photostory.id}`,
+                state: {...this.props}
+              }}><h3>{this.props.photostory.title}</h3></Link>
+              <button onClick={this.deletePhotostory}>Delete Story</button>
+              <Photo photos={this.props.photostory.photos}/>
+            </Segment>)
+
 }
+
+
+}
+
+
 
 export default PhotoStory;
