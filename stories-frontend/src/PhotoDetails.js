e056cd5f08344167af87178542483054483a91ae
github username <torpid@gmail.com>
2018-07-03 16:36:36 -0400

    carousel prototype working

--- stories-frontend/src/PhotoDetails.js
+++ stories-frontend/src/PhotoDetails.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from "react";
 import Photo from "./Photo";
 import { Container, Header, Divider, Segment } from 'semantic-ui-react';
+import { Carousel } from 'react-responsive-carousel';
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 class PhotoDetails extends Component {
 
@@ -14,6 +16,9 @@ class PhotoDetails extends Component {
       <Segment basic>
       <Container text>{this.props.location.state.photostory.description ? <Fragment><Header as='h2'>Description</Header><Divider/>{this.props.location.state.photostory.description}</Fragment> : ""}</Container>
       </Segment>
+      <Carousel>
+        {this.props.location.state.photostory.photos.map(photo => {return <div><img src={`http://localhost:3000${photo.picture.url}`} /></div>})}
+      </Carousel>
       </div>
     )
   }
