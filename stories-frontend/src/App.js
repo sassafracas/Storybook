4ebb86737d6f1099572c94f5abd2c44ca032d7f5
github username <torpid@gmail.com>
2018-08-15 13:27:25 -0400

    need to fix form focus

--- stories-frontend/src/App.js
+++ stories-frontend/src/App.js
@@ -10,6 +10,7 @@ import Explore from "./Explore"
 import MyStories from "./MyStories"
 import Upload from "./Upload"
 import PhotoDetails from "./PhotoDetails"
+import { Form, Input, TextArea, Button, Image, Icon, Message } from 'semantic-ui-react';
 
 class App extends Component {
 
@@ -19,7 +20,17 @@ class App extends Component {
     currentUserId: "",
     photostories: [],
     errors: "",
-    forceRefresh: false,
+    title: "",
+    description: "",
+    caption: "",
+    private: false,
+    picture: [],
+    formErrors: {title: "", caption: "", picture: []},
+    inputPicture: [],
+    titleValid: false,
+    captionValid: false,
+    pictureValid: false,
+    formValid: false
   }
 
   componentDidMount(){
@@ -40,18 +51,104 @@ class App extends Component {
     console.log("hi you're editing ", this.state.photostories);
   }
 
-  componentWillReceiveProps(nextProps) {
-    console.log("next props it will recieve", nextProps.location.pathname);
-    console.log("current props", this.props.location.pathname);
-    if (this.props.location.pathname === "/upload" || this.props.location.pathname === "/login" || this.props.location.pathname === "/register" && nextProps.location.pathname === "/my-stories") {
-      this.forceRefresh()
+
+  //make caption & title input a controlled element (will use for future added inputs)
+    handlePhotoInputChange = (event) => {
+      console.log(this.state);
+      let name = event.target.name
+      let value = event.target.value
+      this.setState({
+        [event.target.name]: event.target.value
+      }, () => { this.validateField(name, value) })
     }
 
-  }
+    validateField = (fieldName, fieldValue) => {
+      let fieldValidationErrors = this.state.formErrors;
+      let titleValid = this.state.titleValid
+      let captionValid = this.state.captionValid
+      let pictureValid = this.state.pictureValid
 
-  forceRefresh = () => {
-    window.location.reload()
-  }
+      switch (fieldName) {
+        case 'title':
+          titleValid = fieldValue.length >= 1;
+          fieldValidationErrors.title = titleValid ? "" : " must be at least one character long";
+          break;
+        case 'caption':
+          captionValid = fieldValue.length >= 1;
+          fieldValidationErrors.caption = captionValid ? "" : " must be at least one character long";
+          break;
+        case 'inputPicture':
+          pictureValid = fieldValue.length >= 1;
+          fieldValidationErrors.picture = pictureValid ? "" : " must have at least one photo selected";
+        default:
+          break;
+      }
+      this.setState({ formErrors: fieldValidationErrors, titleValid, captionValid, pictureValid}, this.validateForm)
+    }
+
+    validateForm() {
+      this.setState({ formValid: this.state.titleValid && this.state.captionValid && this.state.pictureValid})
+    }
+
+    handleRadioButton = (event, value) => {
+      event.persist();
+      console.log(value);
+      this.setState({
+        private: value.value
+      }, ()=> console.log(this.state))
+    }
+  //post state to database
+    handlePhotoUpload = (event) => {
+      event.preventDefault();
+      event.persist();
+
+      console.log("upload event ", event);
+      console.log("Before submit state ", this.state)
+
+      //["[[Target]]"].target[2].files
+      let formData = new FormData();
+      formData.append("caption", this.state.caption)
+      formData.append("title", this.state.title)
+      formData.append("description", this.state.description)
+      formData.append("private", this.state.private)
+      formData.append("picture", event.target[5].files[0])
+      Adapter.postToPhotos(formData)
+        .then(r => r.json())
+        .then(r => this.putPhotoOnScreen(r))
+
+    }
+
+    photoChange = (event, data) => {
+      const name = "inputPicture"
+      const value = data.value
+      this.setState({
+        [name]: value
+      }, () => { this.validateField(name, value) })
+    }
+
+    putPhotoOnScreen = (photoObj) => {
+      console.log("after posting to preview and after uploading to backend", photoObj);
+      this.setState({
+        caption: "",
+        captionValid: false,
+        formValid: false,
+        picture: [...this.state.picture, photoObj]
+      }, ()=> console.log("after setting picture state ", this.state))
+    }
+
+    mapPhotoPreviews = () => {
+      return this.state.picture.map(picture => <Fragment><Image key={picture.id} src={`http://localhost:3000/${picture.picture.url}`} floated="left" bordered centered size="medium"/><Button color="red" size="mini" compact icon basic attached="right" floated="left" onClick={(event, buttonInfo) => this.deletePhotoFromStateAndBackend(event, buttonInfo, picture)}><Icon name="x"/></Button></Fragment>)
+    }
+
+    deletePhotoFromStateAndBackend = (event, buttonInfo, picture) => {
+      event.persist();
+      console.log(event);
+      let deletedPhotoObjIndex = this.state.picture.findIndex((previewPhoto, index) => previewPhoto.id === picture.id)
+      console.log("deleted index ", deletedPhotoObjIndex);
+      this.setState({
+        picture: [...this.state.picture.slice(0, deletedPhotoObjIndex), ...this.state.picture.slice(deletedPhotoObjIndex+1)]
+      }, () => Adapter.deletePreviewPhoto(picture.id))
+    }
 
   getAllUserStories = () => {
     console.log("get all user stories ", this.state);
@@ -125,19 +222,47 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar routeInfo={this.props} clearErrorState={this.clearErrorState}/>
-        <Route exact path="/" component={Welcome} />
+        <Route
+          exact path="/" component={Welcome} />
           { Adapter.isLoggedIn() ?
             <Fragment>
-              <Route exact path="/explore" component={Explore} />
-              <Route exact path="/my-stories" component={(props) => <MyStories {...this.state} history={props.history} deletePhotostory={this.deletePhotostory} editCaptionInState={this.editCaptionInState}  />} />
-              <Route exact path="/upload" component={Upload} />
-              <Route exact path="/my-stories/:id" component={(props) => <PhotoDetails {...this.state} history={props.history} editCaptionInState={this.editCaptionInState}  />} />
+              <Route
+                exact path="/explore"
+                component={Explore} />
+              <Route
+                exact path="/my-stories"
+                component={(props) => <MyStories {...this.state}
+                                        history={props.history}
+                                        deletePhotostory={this.deletePhotostory}
+                                        editCaptionInState={this.editCaptionInState}  />} />
+              <Route
+                exact path="/upload"
+                component={(props) => <Upload {...this.state}
+                                        history={props.history}
+                                        handlePhotoInputChange={this.handlePhotoInputChange}
+                                        validateField={this.validateField}
+                                        handleRadioButton={this.handleRadioButton}
+                                        handlePhotoUpload={this.handlePhotoUpload}
+                                        photoChange={this.photoChange}
+                                        putPhotoOnScreen={this.putPhotoOnScreen}
+                                        mapPhotoPreviews={this.mapPhotoPreviews} />} />
+              <Route
+                exact path="/my-stories/:id"
+                component={(props) => <PhotoDetails {...this.state}
+                                        history={props.history}
+                                        editCaptionInState={this.editCaptionInState}  />} />
             </Fragment>
             :
             <Fragment>
                <Redirect to="/"/>
-              <Route exact path="/register" component={(props) => <RegistrationForm {...props} />} />
-              <Route exact path="/login" component={(props) => <LoginForm {...this.state} {...props} handleLogInSubmit={this.handleLogInSubmit} handleInputChange={this.handleInputChange}/>} />
+              <Route
+                exact path="/register"
+                component={(props) => <RegistrationForm {...props} />} />
+              <Route
+                exact path="/login"
+                component={(props) => <LoginForm {...this.state} {...props}
+                                        handleLogInSubmit={this.handleLogInSubmit}
+                                        handleInputChange={this.handleInputChange}/>} />
             </Fragment>
           }
 
--- stories-frontend/src/Upload.js
+++ stories-frontend/src/Upload.js
@@ -2,192 +2,76 @@ import React, { Component, Fragment } from "react";
 import Adapter from "./Adapter";
 import { Form, Input, TextArea, Button, Image, Icon, Message } from 'semantic-ui-react';
 import { withRouter } from 'react-router-dom';
-
+import { v4 } from "uuid";
 class Upload extends Component {
 
-  state = {
-    title: "",
-    description: "",
-    caption: "",
-    private: false,
-    picture: [],
-    formErrors: {title: "", caption: "", picture: []},
-    inputPicture: [],
-    titleValid: false,
-    captionValid: false,
-    pictureValid: false,
-    formValid: false
-  }
-//make caption & title input a controlled element (will use for future added inputs)
-  handlePhotoInputChange = (event) => {
-    const name = event.target.name
-    const value = event.target.value
-    this.setState({
-      [name]: value
-    }, () => { this.validateField(name, value) })
-  }
 
-  validateField = (fieldName, fieldValue) => {
-    let fieldValidationErrors = this.state.formErrors;
-    let titleValid = this.state.titleValid
-    let captionValid = this.state.captionValid
-    let pictureValid = this.state.pictureValid
-
-    switch (fieldName) {
-      case 'title':
-        titleValid = fieldValue.length >= 1;
-        fieldValidationErrors.title = titleValid ? "" : " must be at least one character long";
-        break;
-      case 'caption':
-        captionValid = fieldValue.length >= 1;
-        fieldValidationErrors.caption = captionValid ? "" : " must be at least one character long";
-        break;
-      case 'inputPicture':
-        pictureValid = fieldValue.length >= 1;
-        fieldValidationErrors.picture = pictureValid ? "" : " must have at least one photo selected";
-      default:
-        break;
-    }
-    this.setState({ formErrors: fieldValidationErrors, titleValid, captionValid, pictureValid}, this.validateForm)
-  }
-
-  validateForm() {
-    this.setState({ formValid: this.state.titleValid && this.state.captionValid && this.state.pictureValid})
-  }
-
-  handleRadioButton = (event, value) => {
-    event.persist();
-    console.log(value);
-    this.setState({
-      private: value.value
-    }, ()=> console.log(this.state))
-  }
-//post state to database
-  handlePhotoUpload = (event) => {
-    event.preventDefault();
-    event.persist();
-
-    console.log("upload event ", event);
-    console.log("Before submit state ", this.state)
-
-    //["[[Target]]"].target[2].files
-    let formData = new FormData();
-    formData.append("caption", this.state.caption)
-    formData.append("title", this.state.title)
-    formData.append("description", this.state.description)
-    formData.append("private", this.state.private)
-    formData.append("picture", event.target[5].files[0])
-    Adapter.postToPhotos(formData)
-      .then(r => r.json())
-      .then(r => this.putPhotoOnScreen(r))
-
-  }
-
-  photoChange = (event, data) => {
-    const name = "inputPicture"
-    const value = data.value
-    this.setState({
-      [name]: value
-    }, () => { this.validateField(name, value) })
-  }
-
-  putPhotoOnScreen = (photoObj) => {
-    console.log("after posting to preview and after uploading to backend", photoObj);
-    this.setState({
-      caption: "",
-      captionValid: false,
-      formValid: false,
-      picture: [...this.state.picture, photoObj]
-    }, ()=> console.log("after setting picture state ", this.state))
-  }
-
-//change state to include picture
-  handlePhotoClick = (event) => {
-    event.persist()
-    console.log("handlePhotoClick before set state", event)
-    this.setState({
-      picture: event.target[3].files["0"]
-    })
-    //["[[Target]]"].target.files["0"]
-
-    //["[[Target]]"].target.files
-    console.log("handlePhotoClick ", event.target.files[0])
-  }
-
-  mapPhotoPreviews = () => {
-    return this.state.picture.map(picture => <Fragment><Image key={picture.id} src={`http://localhost:3000/${picture.picture.url}`} floated="left" bordered centered size="medium"/><Button color="red" size="mini" compact icon basic attached="right" floated="left" onClick={(event, buttonInfo) => this.deletePhotoFromStateAndBackend(event, buttonInfo, picture)}><Icon name="x"/></Button></Fragment>)
-  }
-
-  deletePhotoFromStateAndBackend = (event, buttonInfo, picture) => {
-    event.persist();
-    console.log(event);
-    let deletedPhotoObjIndex = this.state.picture.findIndex((previewPhoto, index) => previewPhoto.id === picture.id)
-    console.log("deleted index ", deletedPhotoObjIndex);
-    this.setState({
-      picture: [...this.state.picture.slice(0, deletedPhotoObjIndex), ...this.state.picture.slice(deletedPhotoObjIndex+1)]
-    }, () => Adapter.deletePreviewPhoto(picture.id))
-  }
 
   render(){
     return(
       <Fragment>
-      <Form onSubmit={this.handlePhotoUpload}>
+      <Form onSubmit={this.props.handlePhotoUpload} key={v4()}>
         <h2>Upload A Photo</h2>
-        <Form.Field required error={!this.state.titleValid}>
+        <Form.Field key={v4()} required error={!this.props.titleValid}>
           <label>Story Title</label>
           <Input
+            key={v4()}
             type="text"
-            value={this.state.title}
-            name="title" placeholder="Story Title"
-            onChange={this.handlePhotoInputChange}></Input>
+            value={this.props.title}
+            name="title"
+            placeholder="Story Title"
+            onChange={this.props.handlePhotoInputChange}></Input>
         </Form.Field>
-        <Form.Field>
+        <Form.Field key={v4()}>
           <label>Story Description</label>
           <TextArea
+            key={v4()}
             type="text"
-            value={this.state.description}
+            value={this.props.description}
             name="description"
             placeholder="Story Text"
-            onChange={this.handlePhotoInputChange}></TextArea>
+            onChange={this.props.handlePhotoInputChange}></TextArea>
         </Form.Field>
-        <Form.Field required error={!this.state.captionValid}>
+        <Form.Field key={v4()} required error={!this.props.captionValid}>
           <label>Caption</label>
           <TextArea
-            value={this.state.caption}
+            key={v4()}
+            value={this.props.caption}
             name="caption"
             placeholder="Photo Caption"
-            onChange={this.handlePhotoInputChange}></TextArea>
+            onChange={this.props.handlePhotoInputChange}></TextArea>
         </Form.Field>
-        {this.state.formErrors.caption ? <Message error header={this.state.formErrors.caption}/> : "" }
+        {this.props.formErrors.caption ? <Message error header={this.props.formErrors.caption}/> : "" }
         <Form.Group inline>
         <Form.Radio
             label='Private'
             value={true}
-            checked={this.state.private === true}
-            onChange={this.handleRadioButton}
+            checked={this.props.private === true}
+            onChange={this.props.handleRadioButton}
           />
           <Form.Radio
             label='Public'
             value={false}
-            checked={this.state.private === false}
-            onChange={this.handleRadioButton}
+            checked={this.props.private === false}
+            onChange={this.props.handleRadioButton}
           />
         </Form.Group>
-        <Form.Field required error={!this.state.pictureValid}>
+        <Form.Field key={v4()} required error={!this.props.pictureValid}>
           <label>Photo</label>
           <Input
+            key={v4()}
             type="file"
             name="picture"
             multiple={true}
             accept="image/*"
-            onChange={(event, data) => this.photoChange(event, data)}
+            onChange={(event, data) => this.props.photoChange(event, data)}
             ></Input>
         </Form.Field>
         <Form.Field>
-          <Button type="submit" disabled={!this.state.formValid}>Upload Your Photo</Button>
+          <Button type="submit" disabled={!this.props.formValid}>Upload Your Photo</Button>
         </Form.Field>
       </Form>
-      {this.state.picture[0] ? this.mapPhotoPreviews() : <h4>Preview of Photos</h4>}
+      {this.props.picture[0] ? this.props.mapPhotoPreviews() : <h4>Preview of Photos</h4>}
       </Fragment>
     )
   }
